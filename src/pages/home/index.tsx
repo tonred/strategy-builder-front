aee6899a5ea044e7e146f12ff719fa8a4bf4bdd4
pavelkhachatrian <pavelkhachatrian@gmail.com>
2022-11-27 18:22:54 +0100

    update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react'
-import { Button, Heading, Main } from 'grommet'
+import {
+    Button, Heading, Main, Paragraph,
+} from 'grommet'
 
 export default function Page(): JSX.Element {
     return (
         <Main pad="large" align="center" gap="medium">
             <Heading>DeFi Strategy Builder</Heading>
-            <Button href="/builder">Build your own strategy</Button>
+            <Button label="Build your own strategy" href="/builder" />
+            <Paragraph>Or try our strategies for examples</Paragraph>
             <Button href="/strategy/0:2ddef82f0bd07ea5013c35f3fde096e3580cba2d9c157d1d241e5725345e654b">
                 Swap WEVER to USDT
             </Button>
-            <Button href="/strategy/0:db1fcae42921c93006639101a1c83c7253ab47100757641b579d87e214b39029">
-                Farm for strategy
+            <Button href="/strategy/0:35b5f401b1923644c93f5fbad2ee902f090febe794cd8cf1067f2794f9bcf362">
+                Swap WEVER to 3 stables
             </Button>
-            <Button href="/strategy/0:e7e80d1c4bb4bdc14ac75f7cc260698d13b8abad783bd6395c8ef2f4646a2cfa">
+            <Button href="/strategy/0:11c828b1c4ff5dd05e352ec1596c8b4c04ebdcfeb9aefa25043f9a1bbf2a7022">
+                Farm WEVER/USDT
+            </Button>
+            <Button href="/strategy/0:7e4d84423acf3121b42113d669fc87be010ff3926d0ba007874663e53a912197">
                 Swap any token to WEVER
+            </Button>
+            <Button href="/strategy/0:25b53ba5ebc425afabff4c5d9dd0772c5a03b838d8d364ab1c25bf57a5fd8d25">
+                Fill WEVER/BRIDGE pool
             </Button>
         </Main>
     )
